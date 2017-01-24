4b109c6d8b9f60d7a0f62cd63592c7bac305dff0
Maxb <maxb.personal@gmail.com>
2017-01-23 16:35:33 -0800

    Removing ProfileActions/ProfileReducer that were leftover from seed project

--- app/client/src/reducers/root.js
+++ app/client/src/reducers/root.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { updateUserInfo } from './AuthReducer';
-import { updateProfileData } from './ProfileReducer';
 
 const RootReducer = combineReducers({
   userAuthSession: updateUserInfo,
-  userProfileData: updateProfileData
 });
 
 export default RootReducer;
