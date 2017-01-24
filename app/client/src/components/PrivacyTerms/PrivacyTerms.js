5a9dcf8334a7413109a16a6c12d4850409cdf304
Maxb <maxb.personal@gmail.com>
2017-01-23 18:22:26 -0800

    Styling Privacy Terms signup page.

--- app/client/src/components/PrivacyTerms/PrivacyTerms.js
+++ app/client/src/components/PrivacyTerms/PrivacyTerms.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux'
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import s from '../LoginForm/LoginForm.scss';
+import loginStyle from '../LoginForm/LoginForm.scss';
+import termsStyle from './PrivacyTerms.scss';
 
 
 class PrivacyTerms extends Component {
@@ -20,11 +21,39 @@ class PrivacyTerms extends Component {
     const { data, agreeToTerms, cancel } = this.props;
 
     return (
-      <div className={s.outerContainer}>
-        <div className={s.innerContainer}>
+      <div className={loginStyle.outerContainer}>
+        <div className={loginStyle.innerContainer}>
           <Paper zDepth={2}>
-            <div className={s.termsContainer}>
-              <h1>Privacy and Terms</h1>
+            <div className={termsStyle.termsContainer}>
+              <h1 className={termsStyle.header}>Privacy and Terms</h1>
+              <p>
+                We’re going to have some pretty special terms, as we’re
+                going to have some very stringent privacy policies, plus
+                here’s where we’re going to need to spell out in no
+                uncertain terms that this is a place for people to engage
+                in positive action, not yelling or attacking etc, and we’re
+                going to use this opportunity to weed out right wing nut
+                jobs and so on...
+              </p>
+              <p>
+                We may want to make people initial certain bits about
+                civility, the way they do when you rent a car.
+              </p>
+              <p>
+                place for people to engage in positive action, not yelling
+                or attacking etc, and we’re going to use this opportunity
+                to weed out right wing nut jobs and so on...
+              </p>
+              <p>
+                We’re going to have some pretty special terms, as we’re
+                going to have some very stringent privacy policies, plus
+                here’s where we’re going to need to spell out in n
+                o uncertain terms that this is a place for people to
+                engage in positive action, not yelling or attacking etc, and
+                we’re going to use this opportunity to weed out right
+                uncertain terms that this is a place for people to engage
+                in positive action, not yelling or attacking etc, and we’re
+              </p>
               <p>
                 We’re going to have some pretty special terms, as we’re
                 going to have some very stringent privacy policies, plus
@@ -55,13 +84,15 @@ class PrivacyTerms extends Component {
               </p>
             </div>
             <RaisedButton 
+              onTouchTap={cancel} 
+              label="Cancel" 
+              className={termsStyle.button}
+            />
+            <RaisedButton 
               onTouchTap={agreeToTerms} 
               primary={true} 
               label="Agree" 
-            />
-            <RaisedButton 
-              onTouchTap={cancel} 
-              label="Cancel" 
+              className={termsStyle.button}
             />
           </Paper>
         </div>
