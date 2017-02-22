41099466139d1f6dde3714b9e1187694781b0b46
Maxb <maxb.personal@gmail.com>
2017-02-22 14:34:34 -0800

    Some shuffling around of docker+postgres+postgis configurations.

--- postgres/create_dbs.sh
+++ postgres/create_dbs.sh
@@ -3,15 +3,15 @@ set -e
 
 echo 'Running postgres entrypoint and creating database'
 
+# TODO: Refactor uprise username into environment variable?
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 psql --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER docker;
-    CREATE DATABASE docker;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
-    CREATE DATABASE prod;
-    GRANT ALL PRIVILEGES ON DATABASE prod TO docker;
-    CREATE DATABASE dev;
-    GRANT ALL PRIVILEGES ON DATABASE dev TO docker;
-    CREATE DATABASE test;
-    GRANT ALL PRIVILEGES ON DATABASE test TO docker;
+    CREATE USER uprise;
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO uprise;
+
+    CREATE SCHEMA app AUTHORIZATION uprise;
+    CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA app;
+    CREATE EXTENSION IF NOT EXISTS "pg_trgm" SCHEMA app;
+
+    ALTER USER uprise SET search_path = uprise, app, public;
 EOSQL
