a7887b75723d1ca378eef8dea2a21243a7541388
Maxb <maxb.personal@gmail.com>
2017-02-23 13:28:53 -0800

    Integrating more of Opportunity geography search into knex language.

--- app/server/models/Opportunity.js
+++ app/server/models/Opportunity.js
@@ -204,14 +204,15 @@ class Opportunity {
 
                 // TODO: It would be nice to refactor some of this out into knex language
                 const distanceQuery = db.select('id')
-                  .from(db.raw(`
-                    (
-                      SELECT opportunities.id, opportunities.title, opportunities.place_name, ST_DISTANCE(opportunities.location, target_zip.location) * 0.000621371192237 AS distance
-                      FROM
-                      (SELECT postal_code, location from zipcodes where postal_code=?) target_zip
-                      CROSS JOIN
-                      (select * from opportunities join zipcodes on zipcodes.postal_code = opportunities.zip) opportunities
-                    ) as q`, zipcode))
+                  .from(function() {
+                    this.select('opportunities.id', db.raw('ST_DISTANCE(opportunities.location, target_zip.location) * ? AS distance', milesInMeter))
+                      .as('distances')
+                      .from(db.raw(`
+                        (SELECT postal_code, location from zipcodes where postal_code=?) target_zip
+                        CROSS JOIN
+                        (select * from opportunities join zipcodes on zipcodes.postal_code = opportunities.zip) opportunities
+                      `, zipcode))
+                  })
                   .where('distance', '<=', distance);
 
                 this.orWhere('id', 'in', distanceQuery);
