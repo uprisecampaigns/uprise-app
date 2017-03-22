66f4d9733977d9b9c2846c05b4967a383b407a8f
Maxb <maxb.personal@gmail.com>
2017-03-21 17:22:37 -0700

    Adding some tests for redux search actions.

---
+++ app/client/src/actions/SearchActions.spec.js
@@ -0,0 +1,61 @@
+import { assert, expect } from 'chai';
+const isEqual = require('lodash.isequal');
+
+import { 
+  ADD_SEARCH_ITEM, 
+  REMOVE_SEARCH_ITEM,
+  SET_DATES,
+  UNSET_DATES,
+  SORT_BY,
+  addSearchItem,
+  removeSearchItem,
+  setSearchDates,
+  unsetSearchDates,
+  sortBy,
+} from 'actions/SearchActions.js';
+
+
+describe('(Actions) SearchActions', () => {
+
+  it('should create an action to add a new search item', () => {
+
+    const newKeyword = 'a new keyword';
+
+    const expectedAction = {
+      type: ADD_SEARCH_ITEM,
+      searchType: 'opportunities',
+      collection: 'keywords',
+      value: newKeyword
+    };
+
+    expect(
+      isEqual(
+        addSearchItem('opportunities', 'keywords', newKeyword), 
+        expectedAction
+      )
+    ).to.be.true;
+
+  });
+
+  it('should create an action to remove a new search item', () => {
+
+    const newKeyword = 'a new keyword';
+
+    const expectedAction = {
+      type: REMOVE_SEARCH_ITEM,
+      searchType: 'opportunities',
+      collection: 'keywords',
+      value: newKeyword
+    };
+
+    expect(
+      isEqual(
+        removeSearchItem('opportunities', 'keywords', newKeyword), 
+        expectedAction
+      )
+    ).to.be.true;
+
+  });
+
+});
+ 
