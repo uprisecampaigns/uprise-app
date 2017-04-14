8ea0a0ccacb28c050ba84956b362ba8ba730e6e3
Maxb <maxb.personal@gmail.com>
2017-04-14 11:40:42 -0700

    Fixing broken tests that needed redux store.

--- app/client/src/components/ComposeMessage/ComposeMessage.spec.js
+++ app/client/src/components/ComposeMessage/ComposeMessage.spec.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { assert, expect } from 'chai';
 import { render, shallow, mount } from 'enzyme';
+
+import { Provider } from 'react-redux';
+
+import configureStore from 'store/configureStore';
 import ComposeMessage from './ComposeMessage';
+
+const store = configureStore();
 
 describe('(Component) ComposeMessage', () => {
   it('renders without exploding', () => {
-    const wrapper = shallow(<ComposeMessage />);
+    const wrapper = shallow(<ComposeMessage store={store}/>);
     expect(wrapper).to.have.lengthOf(1);
   });
 });
--- app/client/src/components/ImageUploader/ImageUploader.spec.js
+++ app/client/src/components/ImageUploader/ImageUploader.spec.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { assert, expect } from 'chai';
 import { render, shallow, mount } from 'enzyme';
+
+import configureStore from 'store/configureStore';
+
 import ImageUploader from './ImageUploader';
+
+const store = configureStore();
 
 describe('(Component) ImageUploader', () => {
   it('renders without exploding', () => {
-    const wrapper = shallow(<ImageUploader />);
+    const wrapper = shallow(<ImageUploader store={store}/>);
     expect(wrapper).to.have.lengthOf(1);
   });
 });
