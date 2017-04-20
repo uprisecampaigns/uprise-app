f6c0348774833b6e866009129b092e2b00502ef0
Maxb <maxb.personal@gmail.com>
2017-04-20 14:37:04 -0700

    Adding antonias sample users.

--- app/server/seeds/initial-data/users.js
+++ app/server/seeds/initial-data/users.js
@@ -1,15 +1,6 @@
+const vaUsers = require('./users/VA-users-4.20.17.json');
 
 module.exports = async (knex) => {
-
-  await knex('users')
-    .where('email', 'test@uprise.org')
-    .orWhere('email', 'john_anderson99@example.com')
-    .orWhere('email', 'george_brown@example.com')
-    .orWhere('email', 'frank_kaplan49@example.com')
-    .orWhere('email', 'marian_shultz19@example.com')
-    .orWhere('email', 'darryl_wade83@example.com')
-    .orWhere('email', 'james_cole67@example.com')
-    .delete();
 
   let testUser = await knex('users').where('email', 'test@uprise.org').select('id');
 
@@ -22,66 +13,27 @@ module.exports = async (knex) => {
         zipcode: '12345',
         password_hash: '$2a$10$/dGp5EQFDO/5pqRSFryjSO3FUq.Rs6svkof/fiSics32mRPU8QCcS' // the password is password!
       }, 
-      {
-        email: 'john_anderson99@example.com',
-        first_name: 'John',
-        last_name: 'Anderson',
-        zipcode: '23219',
-        phone_number: '2154647781',
-      },
-      {
-        email: 'george_brown@example.com',
-        first_name: 'George',
-        last_name: 'Brown',
-        // street_address: '676 Pine St. Apt. 34',
-        // city: 'Richmond',
-        // state: 'VA',
-        zipcode: '23219',
-        phone_number: '2154647781',
-        password_hash: '$2a$10$/dGp5EQFDO/5pqRSFryjSO3FUq.Rs6svkof/fiSics32mRPU8QCcS' // the password is password!
-      },  
-      {
-        email: 'frank_kaplan49@example.com',
-        first_name: 'Frank',
-        last_name: 'Kaplan',
-        zipcode: '23220',
-        phone_number: '3124665959',
-      },
-      {
-        email: 'marian_shultz19@example.com',
-        first_name: 'Marian',
-        last_name: 'Shultz',
-        // street_address: '1628 Lone Mountain Rd.',
-        // city: 'Richmond',
-        // state: 'VA',
-        zipcode: '23220',
-        phone_number: '3124665959',
-        password_hash: '$2a$10$/dGp5EQFDO/5pqRSFryjSO3FUq.Rs6svkof/fiSics32mRPU8QCcS' // the password is password!
-      },
-      {
-        email: 'darryl_wade83@example.com',
-        first_name: 'Darryl',
-        last_name: 'Wade',
-        // street_address: '8185 W Ann Road',
-        // city: 'Richmond',
-        // state: 'VA',
-        zipcode: '23219',
-        phone_number: '5853894484',
-        password_hash: '$2a$10$/dGp5EQFDO/5pqRSFryjSO3FUq.Rs6svkof/fiSics32mRPU8QCcS' // the password is password!
-      },
-      {
-        email: 'james_cole67@example.com',
-        first_name: 'James',
-        last_name: 'Cole',
-        // street_address: '2623 Covington Ln.',
-        // city: 'Richmond',
-        // state: 'VA',
-        zipcode: '23220',
-        phone_number: '8664913667',
-        password_hash: '$2a$10$/dGp5EQFDO/5pqRSFryjSO3FUq.Rs6svkof/fiSics32mRPU8QCcS' // the password is password!
-      },
+
     ], ['id'] );
   }
 
-  return testUser;
+  console.log(testUser);
+
+  const newVaUsers = [];
+
+  for (let userRecord of vaUsers) {
+
+    const dbUser = await knex('users').where('email', userRecord.email).select('id');
+
+    if (dbUser.length === 0) {
+      const newUser = await knex('users').insert([userRecord], ['id']);
+      newVaUsers.push(newUser);
+    } else {
+      newVaUsers.push(dbUser[0]);
+    }
+  }
+
+  console.log(newVaUsers);
+
+  return testUser.concat(newVaUsers);
 };
