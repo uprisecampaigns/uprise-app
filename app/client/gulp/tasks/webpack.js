b1ceed189ebf5ee155f5c0ef30ba144d41595b8e
Maxb <maxb.personal@gmail.com>
2017-05-31 16:53:26 -0700

    Moving initialization of webpack config variables inside gulp webpack task for proper watch functionality.

--- app/client/gulp/tasks/webpack.js
+++ app/client/gulp/tasks/webpack.js
@@ -17,164 +17,165 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 const config = require('config/gulp.js');
 
-const extractTextPlugin = new ExtractTextPlugin('[name].css');
-const occurenceOrderPlugin = new webpack.optimize.OccurrenceOrderPlugin()
-
-const gitCommit = childProcess.execSync('git rev-parse HEAD').toString().trim();
-
-const commonsChunkPlugin = new CommonsChunkPlugin({
-  names: ['fonts-loader'],
-  minChunks: Infinity,
-});
-
-const bundleAnalyzerPlugin = new BundleAnalyzerPlugin();
-
-const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: HtmlWebpackTemplate,
-  title: 'Uprise app',
-  appMountId: 'app', // Generate #app where to mount
-  mobile: true, // Scale page on mobile
-  inject: false // html-webpack-template requires this to work
-})
-
-const definePlugin = new webpack.DefinePlugin({
-  'process.env': {
-    'NODE_ENV': env.development() ? JSON.stringify('development') : JSON.stringify('production'),
-    'SERVER_BASE_URL': '"' + process.env.SERVER_BASE_URL + '"',
-    'CLIENT_BASE_URL': '"' + process.env.CLIENT_BASE_URL + '"',
-    'AWS_S3_REGION': '"' + process.env.AWS_S3_REGION + '"',
-    'AWS_S3_BUCKET_NAME': '"' + process.env.AWS_S3_BUCKET_NAME + '"',
-    'SENTRY_PUBLIC_DSN': '"' + process.env.SENTRY_PUBLIC_DSN + '"',
-    'GIT_COMMIT': '"' + gitCommit + '"',
-  }
-});
-
-config.webpack = {
-  entry: {
-    'index': path.resolve(config.src, 'index'),
-  },
-  output: {
-    filename: '[name]-[hash].js',
-    publicPath: '/',
-    path: path.resolve(config.dest)
-  },
-  devtool: env.development() ? "eval-cheap-module-source-map" : "",
-  module: {
-    loaders: [
-    {
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      include: [ 
-        path.resolve(config.publicRoot), 
-        path.resolve(config.nodeModules, 'camelcase'),
-      ],
-      query: {
-        cacheDirectory: true,
-        presets: ['es2015', 'react', 'stage-3'],
-        plugins: ['transform-runtime', 'transform-class-properties']
-      }
-    },
-    {
-      test: /\.(graphql|gql)$/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader'
-    }, 
-    {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass-loader?sourceMap'
-        // loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass-loader?sourceMap'
-    },
-    { 
-      test: /\.css$/, 
-      loader: 'style-loader!css-loader' 
-    }, 
-    { 
-      test: /\.(png|jpg)$/, 
-      loader: 'url-loader?limit=8192' // inline base64 URLs for <=8k images, direct URLs for the rest
-    }, 
-    {
-      test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-      loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-    },
-    {
-      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-      loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-    },
-    {
-      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-      loader: 'file-loader'
-    },
-    {
-      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-      loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-    },
-    { 
-      test: /\.md$/, 
-      loader: path.resolve(config.publicRoot, 'src', 'lib', 'markdown-loader.js') 
-    },
-    ]
-  },
-  externals: {
-    'react/addons': true,
-    'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true
-  },
-  plugins: env.production() ? [
-    htmlWebpackPlugin,
-    extractTextPlugin,
-    commonsChunkPlugin,
-
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      compress: { 
-        // warnings: true,
-        warnings: false, // Suppress uglification warnings
-        dead_code: true,
-        drop_debugger: true,
-        conditionals: true,
-        evaluate: true,
-        drop_console: true,
-        pure_getters: true,
-        sequences: true,
-        booleans: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
-    }),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0
-    }),
-    definePlugin,
-    occurenceOrderPlugin,
-    new webpack.optimize.AggressiveMergingPlugin()
-  ] : [
-    // bundleAnalyzerPlugin,
-    definePlugin,
-    commonsChunkPlugin,
-    extractTextPlugin,
-    htmlWebpackPlugin,
-    occurenceOrderPlugin,
-    new webpack.optimize.AggressiveMergingPlugin()
-  ],
-
-  bail: env.production(),
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.json'],
-  },
-  node: {
-    fs: 'empty'
-  }
-};
-
 gulp.task('webpack', ['webpack:clean'], (done) => {
+
+  const gitCommit = childProcess.execSync('git rev-parse HEAD').toString().trim();
+
+  const extractTextPlugin = new ExtractTextPlugin('[name].css');
+  const occurenceOrderPlugin = new webpack.optimize.OccurrenceOrderPlugin()
+
+  const commonsChunkPlugin = new CommonsChunkPlugin({
+    names: ['fonts-loader'],
+    minChunks: Infinity,
+  });
+
+  const bundleAnalyzerPlugin = new BundleAnalyzerPlugin();
+
+  const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    template: HtmlWebpackTemplate,
+    title: 'Uprise app',
+    appMountId: 'app', // Generate #app where to mount
+    mobile: true, // Scale page on mobile
+    inject: false // html-webpack-template requires this to work
+  })
+
+  const definePlugin = new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': env.development() ? JSON.stringify('development') : JSON.stringify('production'),
+      'SERVER_BASE_URL': '"' + process.env.SERVER_BASE_URL + '"',
+      'CLIENT_BASE_URL': '"' + process.env.CLIENT_BASE_URL + '"',
+      'AWS_S3_REGION': '"' + process.env.AWS_S3_REGION + '"',
+      'AWS_S3_BUCKET_NAME': '"' + process.env.AWS_S3_BUCKET_NAME + '"',
+      'SENTRY_PUBLIC_DSN': '"' + process.env.SENTRY_PUBLIC_DSN + '"',
+      'GIT_COMMIT': '"' + gitCommit + '"',
+    }
+  });
+
+  config.webpack = {
+    entry: {
+      'index': path.resolve(config.src, 'index'),
+    },
+    output: {
+      filename: '[name]-[hash].js',
+      publicPath: '/',
+      path: path.resolve(config.dest)
+    },
+    devtool: env.development() ? "eval-cheap-module-source-map" : "",
+    module: {
+      loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [ 
+          path.resolve(config.publicRoot), 
+          path.resolve(config.nodeModules, 'camelcase'),
+        ],
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'react', 'stage-3'],
+          plugins: ['transform-runtime', 'transform-class-properties']
+        }
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader'
+      }, 
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass-loader?sourceMap'
+          // loader: 'style-loader!css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass-loader?sourceMap'
+      },
+      { 
+        test: /\.css$/, 
+        loader: 'style-loader!css-loader' 
+      }, 
+      { 
+        test: /\.(png|jpg)$/, 
+        loader: 'url-loader?limit=8192' // inline base64 URLs for <=8k images, direct URLs for the rest
+      }, 
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+      },
+      { 
+        test: /\.md$/, 
+        loader: path.resolve(config.publicRoot, 'src', 'lib', 'markdown-loader.js') 
+      },
+      ]
+    },
+    externals: {
+      'react/addons': true,
+      'react/lib/ExecutionEnvironment': true,
+      'react/lib/ReactContext': true
+    },
+    plugins: env.production() ? [
+      htmlWebpackPlugin,
+      extractTextPlugin,
+      commonsChunkPlugin,
+
+      new webpack.optimize.UglifyJsPlugin({
+        mangle: true,
+        compress: { 
+          // warnings: true,
+          warnings: false, // Suppress uglification warnings
+          dead_code: true,
+          drop_debugger: true,
+          conditionals: true,
+          evaluate: true,
+          drop_console: true,
+          pure_getters: true,
+          sequences: true,
+          booleans: true,
+          unsafe: true,
+          unsafe_comps: true,
+          screw_ie8: true
+        },
+        output: {
+          comments: false
+        }
+      }),
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0
+      }),
+      definePlugin,
+      occurenceOrderPlugin,
+      new webpack.optimize.AggressiveMergingPlugin()
+    ] : [
+      // bundleAnalyzerPlugin,
+      definePlugin,
+      commonsChunkPlugin,
+      extractTextPlugin,
+      htmlWebpackPlugin,
+      occurenceOrderPlugin,
+      new webpack.optimize.AggressiveMergingPlugin()
+    ],
+
+    bail: env.production(),
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.json'],
+    },
+    node: {
+      fs: 'empty'
+    }
+  };
+
   return gulp.src(config.src)
     .pipe(gulpWebpack(config.webpack, webpack))
     .on("error", notify.onError("Error: <%= error.message %>"))
