3382d2023591175b622388950569f4e76d862fce
Maxb <maxb.personal@gmail.com>
2017-06-11 19:15:53 -0700

    First steps implementing new home design.

--- app/client/src/scenes/Home/Home.js
+++ app/client/src/scenes/Home/Home.js
@@ -1,4 +1,10 @@
 import React, { PropTypes } from 'react';
+import FlatButton from 'material-ui/FlatButton';
+
+import Link from 'components/Link';
+
+import s from 'styles/Page.scss';
+
 
 class Home extends React.Component {
   static propTypes = {
@@ -7,7 +13,46 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={s.root}>
+        <div
+          className={s.homeContentSection1}
+        >
+          <div className={s.contentTitle}>Take Action</div>
+          <div className={s.contentText}>
+            UpRise is reforming our political campaign process by putting you back in the drivers' seat. Sign up now to find volunteering opportunities or create a page so volunteers can find you.
+          </div>
+          <div className={s.signupButtonContainer}>
+            <Link
+              useAhref={false}
+              to="/signup"
+            >
+              <FlatButton
+                className={s.signupButton}
+                label="Sign up now"
+              />
+            </Link>
+          </div>
+        </div>
+        <div
+          className={s.homeContentSection2}
+        >
+          <div className={s.contentTitle}>Volunteers</div>
+          <div className={s.contentText}>
+            UpRise is a social network platform you can use to organize your political life: to find, sign up for and create real-world actions on behalf of candidates and issues.
+          </div>
+        </div>
+        <div
+          className={s.homeContentSection3}
+        >
+        <div className={s.contentTitle}>Campaigns</div>
+          <div className={s.contentText}>
+            UpRise will connect you with new volunteers and provide the tools you need to create effective volunteer-powered campaigns.
+          </div>
+        </div>
+        <div
+          className={s.homeContentSection4}
+        >
+        </div>
       </div>
     );
   }
