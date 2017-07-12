213b21b543099fdf523bbc5929a9f4e553c01ad7
Maxb <maxb.personal@gmail.com>
2017-07-11 17:21:29 -0700

    First working implementation of bulk csv action imports.

--- app/client/src/components/CsvUploader/CsvUploader.js
+++ app/client/src/components/CsvUploader/CsvUploader.js
@@ -5,6 +5,8 @@ import {
   Table, TableBody, TableFooter, TableHeader, TableHeaderColumn,
   TableRow, TableRowColumn
 } from 'material-ui/Table';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import FontIcon from 'material-ui/FontIcon';
 import CircularProgress from 'material-ui/CircularProgress';
 import Dropzone from 'react-dropzone';
@@ -19,11 +21,13 @@ class CsvUploader extends React.Component {
     super(props);
     this.state = {
       csvFile: null,
-      rows: null
+      rows: null,
+      selectedHeaders: null
     };
   }
  
   static propTypes = {
+    config: PropTypes.object.isRequired
   };
 
   componentWillReceiveProps(nextProps) {
@@ -53,7 +57,19 @@ class CsvUploader extends React.Component {
       },
       complete: (results, file) => {
         console.log(results);
-        this.setState({ rows: results.data });
+        const rows = Array.from(results.data).map((row) => {
+          const values = Array.from(row);
+          return {
+            selected: true,
+            values
+          };
+        });
+
+        this.setState({ rows });
+
+        const selectedHeaders = Array(results.data[0].length);
+        selectedHeaders.fill('skip');
+        this.setState({ selectedHeaders });
       }
     });
   }
@@ -63,70 +79,149 @@ class CsvUploader extends React.Component {
     event.preventDefault();
     this.setState({ 
       csvFile: null,
-      rows: null
+      rows: null,
+      selectedHeaders: null
     });
   }
 
+  submit = async (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const submissionValues = [];
+    const { config, ...props } = this.props;
+
+    this.state.rows.filter(i => i.selected).forEach((row) => {
+      const newItem = {};
+
+      this.state.selectedHeaders.forEach((selection, index) => {
+        if (selection !== 'skip') {
+          const selectedHeader = config.headers.find(h => h.slug === selection);
+          if (selectedHeader === undefined) {
+            throw new Error('Can\'t find matching header');
+          }
+
+          const value = selectedHeader.processData(row.values[index]);
+          newItem[selectedHeader.slug] = value;
+        }
+      });
+      submissionValues.push(newItem);
+      console.log(newItem);
+    });
+    console.log(submissionValues);
+
+    await config.onSubmit(submissionValues);
+  }
+
+  handleRowSelection = (selectedRows) => {
+    const newRows = Array.from(this.state.rows);
+    if (selectedRows === 'all') {
+      newRows.forEach((row, index) => newRows[index].selected = true);
+    } else if (selectedRows === 'none') {
+      newRows.forEach((row, index) => newRows[index].selected = false);
+    } else {
+      newRows.forEach((row, index) => newRows[index].selected = (selectedRows.includes(index)));
+    }
+
+    this.setState({
+      rows: newRows
+    });
+  }
+
+  handleHeaderChange = (event, index, value) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    const newlySelected = Array.from(this.state.selectedHeaders);
+
+    const testIndex = newlySelected.indexOf(value);
+    if (testIndex !== -1) {
+      newlySelected[testIndex] = 'skip';
+    }
+
+    newlySelected[index] = value;
+
+    this.setState({ selectedHeaders: newlySelected });
+  }
+
   render() {
-    const { ...props } = this.props;
-    const { rows, ...state } = this.state;
+    const { config, ...props } = this.props;
+    const { rows, selectedHeaders, ...state } = this.state;
 
-    return (
+    if (rows && selectedHeaders) {
+      const availableHeaders = Array.from(config.headers);
+      availableHeaders.unshift({
+        title: 'Skip',
+        slug: 'skip',
+      });
 
-      <div>
-        { rows ? (
-          <div>
-            <RaisedButton
-              onTouchTap={this.cancel}
-              primary={true}
-              label="Cancel"
-            />
-            <Table
-              fixedHeader={true}
-              selectable={true}
-              multiSelectable={true}
-              onRowSelection={this.handleRowSelection}
+      return (
+        <div>
+          <RaisedButton
+            onTouchTap={this.cancel}
+            primary={true}
+            label="Cancel"
+          />
+          <Table
+            fixedHeader={true}
+            selectable={true}
+            multiSelectable={true}
+            onRowSelection={this.handleRowSelection}
+          >
+            <TableHeader
+              displaySelectAll={true}
+              adjustForCheckbox={true}
+              enableSelectAll={true}
             >
-              <TableHeader
-                displaySelectAll={true}
-                adjustForCheckbox={true}
-                enableSelectAll={true}
-              >
-                <TableRow>
-                  {rows[0].map((header, index) => (
-                    <TableHeaderColumn key={index}>{header}</TableHeaderColumn>
+              <TableRow>
+                {selectedHeaders.map((selected, index) => (
+                  <TableHeaderColumn key={index}>
+                    <SelectField
+                      value={selected}
+                      onChange={(event, i, value) => this.handleHeaderChange(event, index, value)}
+                    >
+                      {availableHeaders.map((header, index) => (
+                        <MenuItem value={header.slug} primaryText={header.title} />
+                      ))}
+                    </SelectField>
+                  </TableHeaderColumn>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody
+              displayRowCheckbox={true}
+              showRowHover={true}
+              stripedRows={false}
+              deselectOnClickaway={false}
+            >
+              {rows.map((row, index) => (
+                <TableRow key={index} selected={row.selected} selectable={true}>
+                  {row.values.map((cell, index) => (
+                    <TableRowColumn key={index}>{cell}</TableRowColumn>
                   ))}
                 </TableRow>
-              </TableHeader>
-              <TableBody
-                displayRowCheckbox={true}
-                showRowHover={true}
-                stripedRows={false}
-                deselectOnClickaway={false}
-              >
-                {rows.map((row, index) => (
-                  <TableRow key={index} selected={false} selectable={true}>
-                    {row.map((cell, index) => (
-                      <TableRowColumn key={index}>{cell}</TableRowColumn>
-                    ))}
-                  </TableRow>
-                  ))}
-              </TableBody>
-            </Table>
-          </div>
-        ) : (
-          <Dropzone
-            onDrop={this.onDrop}
-            multiple={false}
-          >
-            <div className={s.instructions}>Drag and drop your csv file here, or click to select an file to upload.</div>
+                ))}
+            </TableBody>
+          </Table>
+          <RaisedButton
+            onTouchTap={this.submit}
+            primary={true}
+            label="Submit"
+          />
+        </div>
+      );
+    } else {
+      return (
+        <Dropzone
+          onDrop={this.onDrop}
+          multiple={false}
+        >
+          <div className={s.instructions}>Drag and drop your csv file here, or click to select an file to upload.</div>
 
-            <FontIcon className="material-icons">add_a_photo</FontIcon>
-          </Dropzone>
-        )}
-
-      </div>
-    );
+          <FontIcon className="material-icons">add_a_photo</FontIcon>
+        </Dropzone>
+      );
+    }
   }
 }
 
--- app/client/src/scenes/ManageCampaignUploadActions/ManageCampaignUploadActions.js
+++ app/client/src/scenes/ManageCampaignUploadActions/ManageCampaignUploadActions.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { compose, graphql } from 'react-apollo';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import FontIcon from 'material-ui/FontIcon';
 
 import CsvUploader from 'components/CsvUploader';
@@ -8,8 +9,12 @@ import Link from 'components/Link';
 
 import CampaignQuery from 'schemas/queries/CampaignQuery.graphql';
 import MeQuery from 'schemas/queries/MeQuery.graphql';
+import TypesQuery from 'schemas/queries/TypesQuery.graphql';
+import LevelsQuery from 'schemas/queries/LevelsQuery.graphql';
+import IssueAreasQuery from 'schemas/queries/IssueAreasQuery.graphql';
+import ActivitiesQuery from 'schemas/queries/ActivitiesQuery.graphql';
 
-// import EditCampaignMutation from 'schemas/mutations/EditCampaignMutation.graphql';
+import CreateActionsMutation from 'schemas/mutations/CreateActionsMutation.graphql';
 
 import s from 'styles/Organize.scss';
 
@@ -17,6 +22,7 @@ import s from 'styles/Organize.scss';
 class ManageCampaignUploadActions extends Component {
 
   static PropTypes = {
+    createActionsMutation: PropTypes.func.isRequired,
     campaignSlug: PropTypes.string.isRequired
   }
 
@@ -26,21 +32,167 @@ class ManageCampaignUploadActions extends Component {
 
   render() {
 
-    if (this.props.campaign) {
+    const processString = (string) => string.trim();
+    const processBoolean = (value) => {
+      if (typeof value !== 'string') {
+        throw new Error('Can only process strings to boolean');
+      }
+      const trimmed = value.trim();
+      if (trimmed === '') {
+        return false;
+      }
+      const lowerCased = trimmed.toLowerCase();
+      if (lowerCased === 'true' || lowerCased === 'y' || lowerCased === 'yes') {
+        return true;
+      }
+      return false;
+    }
+
+    const processDate = (value) => {
+      if (typeof value !== 'string') {
+        throw new Error('Can only process strings to dates');
+      }
+      const date = moment(value.trim());
+      if (!date.isValid()) {
+        throw new Error('Date is not valid', date);
+      }
+      return date.format();
+    }
+
+    const processTags = (value) => {
+      if (typeof value !== 'string') {
+        throw new Error('Can only process strings to tags');
+      }
+      return value.split(',').map(i => i.trim());
+    }
+
+    if (this.props.campaign && this.props.issueAreas &&
+        this.props.levels && this.props.activities &&
+        this.props.types) {
+
       const { campaign, user, ...props } = this.props;
+
+      const processRelationships = (collectionName, value) => {
+        if (typeof value !== 'string') {
+          throw new Error('Can only process strings to activities');
+        }
+
+        const slugs = value.split(',').map(i => i.trim().toLowerCase());
+
+        return slugs.map((slug) => props[collectionName].find(a => a.title.toLowerCase() === slug).id);
+      }
 
       const config = {
         headers: [
           {
-            title: 'title',
-            processData: (item) => {
-              console.log(item);
-              return item;
-            },
-          }
+            title: 'Public Title',
+            slug: 'title',
+            processData: processString,
+          },
+          {
+            title: 'Internal Title',
+            slug: 'internalTitle',
+            processData: processString,
+          },
+          {
+            title: 'Virtual',
+            slug: 'virtual',
+            processData: processBoolean,
+          },
+          {
+            title: 'Location Name',
+            slug: 'locationName',
+            processData: processString,
+          },
+          {
+            title: 'Street Address 1',
+            slug: 'streetAddress',
+            processData: processString,
+          },
+          {
+            title: 'Street Address 2',
+            slug: 'streetAddress2',
+            processData: processString,
+          },
+          {
+            title: 'City',
+            slug: 'city',
+            processData: processString,
+          },
+          {
+            title: 'State',
+            slug: 'state',
+            processData: processString,
+          },
+          {
+            title: 'Zipcode',
+            slug: 'zipcode',
+            processData: processString,
+          },
+          {
+            title: 'Location Notes',
+            slug: 'locationNotes',
+            processData: processString,
+          },
+          {
+            title: 'Ongoing',
+            slug: 'ongoing',
+            processData: processBoolean,
+          },
+          {
+            title: 'Start Time',
+            slug: 'startTime',
+            processData: processDate,
+          },
+          {
+            title: 'End Time',
+            slug: 'endTime',
+            processData: processDate,
+          },
+          {
+            title: 'Keywords',
+            slug: 'tags',
+            processData: processTags,
+          },
+          {
+            title: 'Activities',
+            slug: 'activities',
+            processData: (values) => processRelationships('activities', values),
+          },
+          {
+            title: 'Campaign Types',
+            slug: 'types',
+            processData: (values) => processRelationships('types', values),
+          },
+          {
+            title: 'Campaign Levels',
+            slug: 'levels',
+            processData: (values) => processRelationships('levels', values),
+          },
+          {
+            title: 'Issue Areas',
+            slug: 'issueAreas',
+            processData: (values) => processRelationships('issueAreas', values),
+          },
         ],
-        onSubmit: (data) => {
+        onSubmit: async (data) => {
           console.log(data);
+          const newActions = data.map((action) => {
+            return {
+              campaignId: campaign.id,
+              ...action
+            };
+          });
+
+          console.log(newActions);
+          const results = await this.props.createActionsMutation({
+            variables: {
+              data: newActions
+            },
+            refetchQueries: ['SearchActionsQuery', 'CampaignQuery']
+          });
+
+          console.log(results);
         }
       };
 
@@ -70,6 +222,19 @@ class ManageCampaignUploadActions extends Component {
   }
 }
 
+const graphqlOptions = (collection) => {
+  return {
+    props: ({ data }) => ({
+      [collection]: !data.loading && data[collection] ? data[collection] : []
+    })
+  };
+};
+
+const withActivitiesQuery = graphql(ActivitiesQuery, graphqlOptions('activities'));
+const withTypesQuery = graphql(TypesQuery, graphqlOptions('types'));
+const withLevelsQuery = graphql(LevelsQuery, graphqlOptions('levels'));
+const withIssueAreasQuery = graphql(IssueAreasQuery, graphqlOptions('issueAreas'));
+
 const withMeQuery = graphql(MeQuery, {
   props: ({ data }) => ({
     user: !data.loading && data.me ? data.me : {
@@ -97,5 +262,9 @@ export default compose(
   connect(),
   withMeQuery,
   withCampaignQuery,
-  // graphql(EditCampaignMutation, { name: 'editCampaignMutation' })
+  withActivitiesQuery,
+  withTypesQuery,
+  withLevelsQuery,
+  withIssueAreasQuery,
+  graphql(CreateActionsMutation, { name: 'createActionsMutation' })
 )(ManageCampaignUploadActions);
