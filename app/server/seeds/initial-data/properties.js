402fafa8728acaa529c2489deb435174c2703e8a
Maxb <maxb.personal@gmail.com>
2017-08-08 13:08:23 -0700

    Changing activities list.

--- app/server/seeds/initial-data/properties.js
+++ app/server/seeds/initial-data/properties.js
@@ -36,30 +36,37 @@ module.exports = async (knex, users) => {
   ], ['id']);
 
   const activities = await knex('activities').insert([
-    { title: 'meet', description: 'General meeting' }, // 0 
-    { title: 'phone', description: 'Calling voters' }, // 1 
-    { title: 'canvass', description: 'Door to door' }, // 2 
-    { title: 'outreach', description: 'Public outreach' }, // 3 
-    { title: 'rallies', description: 'Rallies, marches' }, // 4 
-    { title: 'visibility', description: 'Visibility, visual arts' }, // 5 
-    { title: 'neighborhood', description: 'Organize neighborhood' }, // 6
-    { title: 'issue groups', description: 'Organize issue groups' }, // 7
-    { title: 'host', description: 'Host an event' }, // 8 
-    { title: 'event', description: 'Event production' }, // 9 
-    { title: 'policy', description: 'Policy research' }, // 10 
-    { title: 'communication', description: 'Communication' }, // 11
-    { title: 'polling', description: 'Polling, analytics' }, // 12 
-    { title: 'writing', description: 'Writing, editing' }, // 13 
-    { title: 'press', description: 'Press relations' }, // 14 
-    { title: 'social', description: 'Email, social media' }, // 15 
-    { title: 'website', description: 'Website, software' }, // 16 
-    { title: 'design', description: 'Design, production' }, // 17 
-    { title: 'coordinate', description: 'Coordinate volunteers' }, // 18 
-    { title: 'office', description: 'Office admin' }, // 19 
-    { title: 'data', description: 'Data management' }, // 20 
-    { title: 'legal', description: 'Legal' }, // 21 
-    { title: 'accounting', description: 'Accounting' }, // 22
-    { title: 'finance', description: 'Fundraising' }, // 23 
+    { title: 'meet', description: 'Meeting' }, // 0
+    { title: 'phone', description: 'Calling voters' }, // 1
+    { title: 'canvass', description: 'Door to door' }, // 2
+    { title: 'outreach', description: 'Outreach, tabling, street teams' }, // 3
+    { title: 'protest', description: 'Protest, demonstration' }, // 4
+    { title: 'visibility', description: 'Lawn signs, visibility' }, // 5
+    { title: 'precinct', description: 'Organize precinct, neighborhood' }, // 6
+    { title: 'issuegroups', description: 'Organize issue groups' }, // 7
+    { title: 'host', description: 'Host a meet and greet' }, // 8
+    { title: 'event', description: 'Big event production' }, // 9
+    { title: 'policy', description: 'Policy' }, // 10
+    { title: 'communications', description: 'Communications' }, // 11
+    { title: 'writing', description: 'Writing, editing' }, // 12
+    { title: 'press', description: 'Press relations' }, // 13
+    { title: 'social', description: 'Email, social media' }, // 14
+    { title: 'website', description: 'Website, software' }, // 15
+    { title: 'design', description: 'Art, design, audio, video' }, // 16
+    { title: 'coordinate', description: 'Coordinate volunteers' }, // 17
+    { title: 'office', description: 'Office admin' }, // 18
+    { title: 'data', description: 'Data management' }, // 19
+    { title: 'legal', description: 'Legal, voter protection' }, // 20
+    { title: 'bookkeeping', description: 'Bookkeeping' }, // 21
+    { title: 'fundraising', description: 'Fundraising' }, // 22
+    { title: 'training', description: 'Training' }, // 23
+    { title: 'drive', description: 'Drive volunteers, voters' }, // 24
+    { title: 'feed', description: 'Feed volunteers' }, // 25
+    { title: 'lobby', description: 'Lobbying' }, // 26
+    { title: 'research', description: 'Online research' }, // 27
+    { title: 'tech', description: 'Tech, software, coding' }, // 28
+    { title: 'house', description: 'Volunteer housing' }, // 29
+
   ], ['id', 'title', 'description']);
 
 
