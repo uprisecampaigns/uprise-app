c2406f9d913d04a91c277f67b909bda9b181f97e
Maxb <maxb.personal@gmail.com>
2017-11-06 13:42:26 -0800

    Changing migration to actually migrate data into new shifts and shift_signups collections.

--- app/server/migrations/20170925165928_shifts.js
+++ app/server/migrations/20170925165928_shifts.js
@@ -29,11 +29,53 @@ exports.up = async (knex, Promise) => {
 
     table.primary(['shift_id', 'user_id']);
   });
+
+  const actions = await knex('actions').select().where('ongoing', false);
+
+  for (const action of actions) {
+    const shift = {
+      action_id: action.id,
+      start: action.start_time,
+      end: action.end_time,
+    }
+
+    const shiftId = (await knex('shifts').insert(shift, 'id'))[0];
+    await knex('actions').where('id', action.id).update({ start_time: null, end_time: null });
+
+    const signups = await knex.select().table('action_signups').where('action_id', action.id);
+
+    for (const signup of signups) {
+      await knex('shift_signups').insert({
+        shift_id: shiftId,
+        user_id: signup.user_id,
+      });
+    }
+
+    await knex('action_signups').where('action_id', action.id).del();
+  }
 };
 
 exports.down = async (knex, Promise) => {
-  await knex.schema.dropTableIfExists('shifts');
+  const shifts = await knex('shifts').select();
+  // No way to perfectly map 'shifts' back onto start_time and end_time, but we can do it for just one shift
+  // (likely rollback scenario)
+  for (const shift of shifts) {
+    await knex('actions').where('id', shift.action_id).update({ start_time: shift.start, end_time: shift.end });
+  }
+
+  const signups = await knex('shift_signups')
+    .join('shifts', 'shifts.id', '=', 'shift_signups.shift_id')
+    .select('shifts.action_id as action_id', 'shift_signups.user_id as user_id');
+
+  for (const signup of signups) {
+    await knex('action_signups').insert({
+      user_id: signup.user_id,
+      action_id: signup.action_id,
+    });
+  }
+
   await knex.schema.dropTableIfExists('shift_signups');
+  await knex.schema.dropTableIfExists('shifts');
 };
 
 module.exports.configuration = { transaction: true };
