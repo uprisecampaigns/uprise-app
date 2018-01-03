96d76008fa84ed09e32be1b21765d2a162418726
Maxb <maxb.personal@gmail.com>
2018-01-03 14:15:21 -0800

    Handle render error in ActionProfile.

--- app/client/src/components/ActionProfile/ActionProfile.js
+++ app/client/src/components/ActionProfile/ActionProfile.js
@@ -42,137 +42,141 @@ class ActionProfile extends PureComponent {
       const hasUpcomingShifts = (action.shifts &&
         action.shifts.filter(shift => moment(shift.end).isAfter(moment())).length > 0);
 
-      return (
-        <div className={s.outerContainer}>
+      try {
+        return (
+          <div className={s.outerContainer}>
 
-          { action.isOwner && (
-            <Link to={`/organize/${action.campaign.slug}/opportunity/${action.slug}`}>
-              <div className={s.navHeader}>
-                <FontIcon
-                  className={['material-icons', s.backArrow].join(' ')}
-                >arrow_back
-                </FontIcon>
-                {action.title}
-              </div>
-            </Link>
-          )}
-
-          <div className={s.innerContainer}>
-
-            <div className={s.actionTitleSignupContainer}>
-              <div className={s.actionHeaderContainer}>
-                <div className={s.actionTitle}>{action.title}</div>
-                <Link to={`/campaign/${action.campaign.slug}`}>
-                  <div className={s.actionSubheader}>{action.campaign.title}</div>
-                </Link>
-              </div>
-              <SignupRegisterLogin
-                className={s.desktopSignupRegisterLogin}
-                action={action}
-              />
-            </div>
-
-            <div className={s.alternatingBackgroundsContainer}>
-
-              <div className={s.actionDetailsContainer}>
-                <div className={s.actionDetailsTitle}>About this Volunteer Event</div>
-                { (typeof action.description === 'string' && action.description.trim() !== '') &&
-                  <div className={s.actionDetailsContent}>{action.description}</div>
-                }
-
-                <div className={s.detailsNumbersContainer}>
-                  <div className={s.detailsNumbers}>
-                    <div className={s.detailsNumbersHeader}>Keywords</div>
-                    <div className={s.detailsNumbersNumber}>{action.tags.length}</div>
-                  </div>
-
-                  { action.distance && (
-                    <div className={s.detailsNumbers}>
-                      <div className={s.detailsNumbersHeader}>Miles away</div>
-                      <div className={s.detailsNumbersNumber}>{action.distance}</div>
-                    </div>
-                  )}
+            { action.isOwner && (
+              <Link to={`/organize/${action.campaign.slug}/opportunity/${action.slug}`}>
+                <div className={s.navHeader}>
+                  <FontIcon
+                    className={['material-icons', s.backArrow].join(' ')}
+                  >arrow_back
+                  </FontIcon>
+                  {action.title}
                 </div>
+              </Link>
+            )}
+
+            <div className={s.innerContainer}>
+
+              <div className={s.actionTitleSignupContainer}>
+                <div className={s.actionHeaderContainer}>
+                  <div className={s.actionTitle}>{action.title}</div>
+                  <Link to={`/campaign/${action.campaign.slug}`}>
+                    <div className={s.actionSubheader}>{action.campaign.title}</div>
+                  </Link>
+                </div>
+                <SignupRegisterLogin
+                  className={s.desktopSignupRegisterLogin}
+                  action={action}
+                />
               </div>
 
-              { (action.locationName || action.streetAddress || (action.city && action.state && action.zipcode)) && (
+              <div className={s.alternatingBackgroundsContainer}>
+
                 <div className={s.actionDetailsContainer}>
-                  <div className={s.actionDetailsTitle}>
-                    Location
-                  </div>
-                  <div className={s.actionDetailsContent}>
-                    {action.locationName &&
-                      <div>{action.locationName}</div>
-                    }
+                  <div className={s.actionDetailsTitle}>About this Volunteer Event</div>
+                  { (typeof action.description === 'string' && action.description.trim() !== '') &&
+                    <div className={s.actionDetailsContent}>{action.description}</div>
+                  }
 
-                    {action.streetAddress &&
-                      <div>{action.streetAddress}</div>
-                    }
+                  <div className={s.detailsNumbersContainer}>
+                    <div className={s.detailsNumbers}>
+                      <div className={s.detailsNumbersHeader}>Keywords</div>
+                      <div className={s.detailsNumbersNumber}>{action.tags.length}</div>
+                    </div>
 
-                    {action.streetAddress2 &&
-                      <div>{action.streetAddress2}</div>
-                    }
-
-                    { (action.city && action.state && action.zipcode) && (
-                      <div>
-                        {action.city}, {action.state} {action.zipcode}
+                    { action.distance && (
+                      <div className={s.detailsNumbers}>
+                        <div className={s.detailsNumbersHeader}>Miles away</div>
+                        <div className={s.detailsNumbersNumber}>{action.distance}</div>
                       </div>
                     )}
-
-                    {action.locationNotes &&
-                      <div>{action.locationNotes}</div>
-                    }
                   </div>
                 </div>
-              )}
 
-              { activities.length > 0 && (
-                <div className={s.actionDetailsContainer}>
-                  <div className={s.actionDetailsTitle}>
-                    Activities and Skills Needed:
-                  </div>
-                  <div className={s.actionDetailsContent}>{activities}</div>
-                </div>
-              )}
+                { (action.locationName || action.streetAddress || (action.city && action.state && action.zipcode)) && (
+                  <div className={s.actionDetailsContainer}>
+                    <div className={s.actionDetailsTitle}>
+                      Location
+                    </div>
+                    <div className={s.actionDetailsContent}>
+                      {action.locationName &&
+                        <div>{action.locationName}</div>
+                      }
 
-              { keywords && (
-                <div className={s.actionDetailsContainer}>
-                  <div className={s.actionDetailsTitle}>
-                    Keywords:
-                  </div>
-                  <div className={s.actionDetailsContent}>{keywords}</div>
-                </div>
-              )}
+                      {action.streetAddress &&
+                        <div>{action.streetAddress}</div>
+                      }
 
-              { hasUpcomingShifts && (
-                <div className={s.actionDetailsContainer}>
-                  <div className={s.actionDetailsTitle}>
-                  Shift Schedule
-                  </div>
-                  <div className={s.actionDetailsContent}>
-                    <ShiftGroupList action={action} s={s} />
-                  </div>
-                </div>
-              )}
+                      {action.streetAddress2 &&
+                        <div>{action.streetAddress2}</div>
+                      }
 
-              { action.owner && (
-                <div className={s.actionDetailsContainer}>
-                  <div className={s.actionDetailsTitle}>
-                    Get in touch
-                  </div>
-                  <div>{action.owner.first_name} {action.owner.last_name} - Contact Coordinator</div>
-                  <div>
-                    <Link to={`mailto:${action.owner.email}`} mailTo external useAhref>
-                      {action.owner.email}
-                    </Link>
-                  </div>
-                </div>
-              )}
+                      { (action.city && action.state && action.zipcode) && (
+                        <div>
+                          {action.city}, {action.state} {action.zipcode}
+                        </div>
+                      )}
 
+                      {action.locationNotes &&
+                        <div>{action.locationNotes}</div>
+                      }
+                    </div>
+                  </div>
+                )}
+
+                { activities.length > 0 && (
+                  <div className={s.actionDetailsContainer}>
+                    <div className={s.actionDetailsTitle}>
+                      Activities and Skills Needed:
+                    </div>
+                    <div className={s.actionDetailsContent}>{activities}</div>
+                  </div>
+                )}
+
+                { keywords && (
+                  <div className={s.actionDetailsContainer}>
+                    <div className={s.actionDetailsTitle}>
+                      Keywords:
+                    </div>
+                    <div className={s.actionDetailsContent}>{keywords}</div>
+                  </div>
+                )}
+
+                { hasUpcomingShifts && (
+                  <div className={s.actionDetailsContainer}>
+                    <div className={s.actionDetailsTitle}>
+                    Shift Schedule
+                    </div>
+                    <div className={s.actionDetailsContent}>
+                      <ShiftGroupList action={action} s={s} />
+                    </div>
+                  </div>
+                )}
+
+                { action.owner && (
+                  <div className={s.actionDetailsContainer}>
+                    <div className={s.actionDetailsTitle}>
+                      Get in touch
+                    </div>
+                    <div>{action.owner.first_name} {action.owner.last_name} - Contact Coordinator</div>
+                    <div>
+                      <Link to={`mailto:${action.owner.email}`} mailTo external useAhref>
+                        {action.owner.email}
+                      </Link>
+                    </div>
+                  </div>
+                )}
+
+              </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      } catch (e) {
+        console.error(`Error rendering ActionProfile: ${e.message}`);
+      }
     }
     return null;
   }
