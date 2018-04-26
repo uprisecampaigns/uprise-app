3547ea3aa885952780bea3ca8c2525737c43df13
Maxb <maxb.personal@gmail.com>
2018-04-25 21:18:18 -0700

    Removing old seed files. Updating activities seed file. It's the only one really still necessary.

--- app/server/seeds/initial-data.js
+++ app/server/seeds/initial-data.js
@@ -1,14 +1,9 @@
 /* eslint-disable */
 
 const initProperties = require('./initial-data/properties.js');
-const initUsers = require('./initial-data/users.js');
-const initCampaigns = require('./initial-data/campaigns.js');
-const initActions = require('./initial-data/actions.js');
 
 exports.seed = async (knex) => {
-  const {
-    issueAreas, types, levels, activities,
-  } = await initProperties(knex);
+  await initProperties(knex);
   // const users = await initUsers(knex);
   // const { campaigns } = await initCampaigns({ knex, users, issueAreas, types, levels, activities });
   // const actions = await initActions({ knex, users, campaigns, levels, issueAreas, types });
