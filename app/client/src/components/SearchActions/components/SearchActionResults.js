3cfa4aa6a51a4e155ade451a0241ce5bbaac8d6e
Jon Hughes <github@validatorian.com>
2018-07-03 10:25:48 -0700

    Only infinite scroll after user clicks 'show more'

--- app/client/src/components/SearchActions/components/SearchActionResults.js
+++ app/client/src/components/SearchActions/components/SearchActionResults.js
@@ -14,7 +14,6 @@ import Link from 'components/Link';
 
 import s from 'styles/Search.scss';
 
-
 class SearchActionResults extends Component {
   static propTypes = {
     actions: PropTypes.arrayOf(PropTypes.object),
@@ -25,104 +24,131 @@ class SearchActionResults extends Component {
     handleInfiniteLoad: PropTypes.func.isRequired,
     allItemsLoaded: PropTypes.bool,
     timeWithZone: PropTypes.func.isRequired,
-  }
+  };
 
   static defaultProps = {
     actions: undefined,
     cursor: undefined,
     allItemsLoaded: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showAll: false,
+    };
   }
 
-  shouldComponentUpdate(nextProps) {
-    return ((((!nextProps.graphqlLoading && this.props.graphqlLoading) ||
-             (!isEqual(this.props.cursor, nextProps.cursor))) &&
-             typeof nextProps.actions === 'object') ||
-            !isEqual(this.props.sortBy, nextProps.sortBy));
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      (((!nextProps.graphqlLoading && this.props.graphqlLoading) || !isEqual(this.props.cursor, nextProps.cursor)) &&
+        typeof nextProps.actions === 'object') ||
+      !isEqual(this.props.sortBy, nextProps.sortBy) ||
+      (!this.state.showAll && nextState.showAll)
+    );
   }
 
   elementInfiniteLoad = () => (
     <div className={s.loadingContainer}>
-      <CircularProgress
-        size={60}
-        thickness={5}
-      />
+      <CircularProgress size={60} thickness={5} />
     </div>
-  )
+  );
+
+  showAll = (event) => {
+    event.preventDefault();
+    this.setState({ showAll: true });
+  };
 
   render() {
-    const {
-      sortBy, isInfiniteLoading, allItemsLoaded, handleInfiniteLoad, timeWithZone, ...props
-    } = this.props;
+    const { sortBy, isInfiniteLoading, allItemsLoaded, handleInfiniteLoad, timeWithZone, ...props } = this.props;
+    const { showAll } = this.state;
 
-    const actions = props.actions ? Array.from(props.actions).sort(itemsSort(sortBy)).map((action, actionIndex) => {
-      // TODO: better datetime parsing (not relying on native Date) and checking for missing values
-      const startTime = moment(action.start_time);
-      const endTime = moment(action.end_time);
+    const actions = props.actions
+      ? Array.from(props.actions)
+          .sort(itemsSort(sortBy))
+          .map((action, actionIndex) => {
+            // TODO: better datetime parsing (not relying on native Date) and checking for missing values
+            const startTime = moment(action.start_time);
+            const endTime = moment(action.end_time);
 
-      const startTimeString = timeWithZone(startTime, action.zipcode, 'h:mma');
-      const endTimeString = timeWithZone(endTime, action.zipcode, 'h:mma z');
+            const startTimeString = timeWithZone(startTime, action.zipcode, 'h:mma');
+            const endTimeString = timeWithZone(endTime, action.zipcode, 'h:mma z');
 
-      try {
-        return (
-          <Link key={JSON.stringify(action)} to={`/opportunity/${action.slug}`}>
-            <Card className={s.card}>
-              <CardHeader
-                title={action.title}
-                className={s.resultsHeader}
-                avatar={action.campaign.profile_image_url ? <Avatar src={action.campaign.profile_image_url} size={40} /> : undefined}
-              >
-                <div><Link to={`/campaign/${action.campaign.slug}`} className={s.subheaderContainer}>{action.campaign.title}</Link></div>
-                { !action.ongoing && startTime && (
-                  <div>
-                    <div>Date: {timeWithZone(startTime, action.zipcode, 'ddd MMM Do, YYYY')}</div>
-                    <div>Time: {`${startTimeString} - ${endTimeString}`}</div>
-                  </div>
-                )}
-                { action.ongoing && (
-                  <div>Ongoing Role</div>
-                )}
-                { action.virtual && (
-                  <div>Virtual Action</div>
-                )}
-                { (action.city && action.state) && <div>Place: {action.city}, {action.state}</div> }
-                { action.distance && (
-                  <div>{action.distance} miles away</div>
-                )}
-              </CardHeader>
-            </Card>
-          </Link>
-        );
-      } catch (e) {
-        console.error(`Error rendering search action result: ${e.message}`);
-        return null;
-      }
-    }) : [];
+            try {
+              return (
+                <Link key={JSON.stringify(action)} to={`/opportunity/${action.slug}`}>
+                  <Card className={s.card}>
+                    <CardHeader
+                      title={action.title}
+                      className={s.resultsHeader}
+                      avatar={
+                        action.campaign.profile_image_url ? (
+                          <Avatar src={action.campaign.profile_image_url} size={40} />
+                        ) : (
+                          undefined
+                        )
+                      }
+                    >
+                      <div>
+                        <Link to={`/campaign/${action.campaign.slug}`} className={s.subheaderContainer}>
+                          {action.campaign.title}
+                        </Link>
+                      </div>
+                      {!action.ongoing &&
+                        startTime && (
+                          <div>
+                            <div>Date: {timeWithZone(startTime, action.zipcode, 'ddd MMM Do, YYYY')}</div>
+                            <div>Time: {`${startTimeString} - ${endTimeString}`}</div>
+                          </div>
+                        )}
+                      {action.ongoing && <div>Ongoing Role</div>}
+                      {action.virtual && <div>Virtual Action</div>}
+                      {action.city &&
+                        action.state && (
+                          <div>
+                            Place: {action.city}, {action.state}
+                          </div>
+                        )}
+                      {action.distance && <div>{action.distance} miles away</div>}
+                    </CardHeader>
+                  </Card>
+                </Link>
+              );
+            } catch (e) {
+              console.error(`Error rendering search action result: ${e.message}`);
+              return null;
+            }
+          })
+      : [];
 
     return (
       <div>
-        {
-          props.graphqlLoading ? (
-            <div className={s.loadingContainer}>
-              <CircularProgress
-                size={100}
-                thickness={5}
-              />
-            </div>
-          ) : (
-
-            <Infinite
-              elementHeight={140}
-              useWindowAsScrollContainer
-              onInfiniteLoad={handleInfiniteLoad}
-              isInfiniteLoading={isInfiniteLoading}
-              infiniteLoadBeginEdgeOffset={allItemsLoaded ? undefined : 200}
-              loadingSpinnerDelegate={this.elementInfiniteLoad()}
-            >
-              {actions}
-            </Infinite>
-
-          )
-        }
+        {props.graphqlLoading ? (
+          <div className={s.loadingContainer}>
+            <CircularProgress size={100} thickness={5} />
+          </div>
+        ) : showAll ? (
+          <Infinite
+            elementHeight={140}
+            useWindowAsScrollContainer
+            onInfiniteLoad={handleInfiniteLoad}
+            isInfiniteLoading={isInfiniteLoading}
+            infiniteLoadBeginEdgeOffset={allItemsLoaded ? undefined : 200}
+            loadingSpinnerDelegate={this.elementInfiniteLoad()}
+          >
+            {actions}
+          </Infinite>
+        ) : (
+          <div>
+            {actions.slice(0, 15)}
+            {actions.length > 15 && (
+              <div className={s.button} onClick={this.showAll} onKeyPress={this.showAll} role="button" tabIndex="0">
+                Show More
+              </div>
+            )}
+          </div>
+        )}
       </div>
     );
   }
