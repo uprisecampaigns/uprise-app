c637d871c74b64f606063b39e968bec42ef7aac6
Jon Hughes <github@validatorian.com>
2018-07-06 11:48:20 -0700

    Set active section to 'organize' when recruiting

--- app/client/src/routes/searchUsers/index.js
+++ app/client/src/routes/searchUsers/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Layout from 'components/Layout';
 import Home from 'scenes/Home';
 
-import { setPage } from 'actions/PageNavActions';
+import { setPage, setRole } from 'actions/PageNavActions';
 
 export default {
   path: '/search/search-volunteers',
 
   action(context) {
     context.store.dispatch(setPage('user'));
+    context.store.dispatch(setRole('organize'));
 
     return {
       title: 'Search for UpRise Volunteers',
