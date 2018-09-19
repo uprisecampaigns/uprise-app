d2a58493fcacc2b1a6ad84023107bdb50fd77e92
Jon Hughes <github@validatorian.com>
2018-09-19 13:14:55 -0700

    Expand/collapse sections when clicking section header not just carat

--- app/client/src/components/ControlledListItem/ControlledListItem.js
+++ app/client/src/components/ControlledListItem/ControlledListItem.js
@@ -4,18 +4,17 @@ import { ListItem } from 'material-ui/List';
 import NavigationExpandMore from 'material-ui/svg-icons/navigation/expand-more';
 import NavigationExpandLess from 'material-ui/svg-icons/navigation/expand-less';
 
-
 import s from './ControlledListItem.scss';
 
 class ControlledListItem extends PureComponent {
   static propTypes = {
     className: PropTypes.string.isRequired,
     initiallyOpen: PropTypes.bool,
-  }
+  };
 
   static defaultProps = {
     initiallyOpen: false,
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -31,24 +30,16 @@ class ControlledListItem extends PureComponent {
     const { className, ...props } = this.props;
 
     const handleClick = (event) => {
+      console.log('test', props, this.props);
       event.preventDefault();
       event.stopPropagation();
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         open: !prevState.open,
       }));
     };
 
     return (
-      <ListItem
-        open={open}
-        className={[className].concat([s.listItem]).join(' ')}
-        rightToggle={open ?
-          <NavigationExpandLess onClick={handleClick} /> :
-          <NavigationExpandMore onClick={handleClick} />
-        }
-        onClick={handleClick}
-        {...props}
-      />
+      <ListItem open={open} className={[className].concat([s.listItem]).join(' ')} onClick={handleClick} {...props} />
     );
   }
 }
