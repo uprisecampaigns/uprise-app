d504e7de4b12e9f03112353c874afcadf28c693b
Jon Hughes <github@validatorian.com>
2018-11-14 13:38:37 -0800

    Make 'My Opportunities' and 'Followed Campaigns' point to volunteer in header

--- app/client/src/routes/actionCommitments/index.js
+++ app/client/src/routes/actionCommitments/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import ActionCommitments from 'scenes/ActionCommitments';
 import Layout from 'components/Layout';
 
-import { setRole } from 'actions/PageNavActions';
+import { setPage } from 'actions/PageNavActions';
 
 export default {
-
   path: '/volunteer/opportunity-commitments',
 
   action({ store }) {
-    store.dispatch(setRole('user'));
+    store.dispatch(setPage('action'));
     return {
       title: 'Opportunity Commitments',
-      component: <Layout><ActionCommitments /></Layout>,
+      component: (
+        <Layout>
+          <ActionCommitments />
+        </Layout>
+      ),
     };
   },
 };
--- app/client/src/routes/campaignSubscriptions/index.js
+++ app/client/src/routes/campaignSubscriptions/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import CampaignSubscriptions from 'scenes/CampaignSubscriptions';
 import Layout from 'components/Layout';
 
-import { setRole } from 'actions/PageNavActions';
+import { setPage } from 'actions/PageNavActions';
 
 export default {
-
   path: '/volunteer/campaign-subscriptions',
 
   action({ store }) {
-    store.dispatch(setRole('user'));
+    store.dispatch(setPage('action'));
     return {
       title: 'My Subscriptions',
-      component: <Layout><CampaignSubscriptions /></Layout>,
+      component: (
+        <Layout>
+          <CampaignSubscriptions />
+        </Layout>
+      ),
     };
   },
 };
