a7a5ce8e85b15b360d3087893d8059041df71e6a
Jon Hughes <github@validatorian.com>
2018-11-14 13:25:20 -0800

    Reorganize 'volunteer' page

--- app/client/src/scenes/Volunteer/Volunteer.js
+++ app/client/src/scenes/Volunteer/Volunteer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'components/Link';
 
 import Commitments from '../ActionCommitments';
 import Subscriptions from '../CampaignSubscriptions';
@@ -15,14 +16,30 @@ function Volunteer(props) {
 
         <div className={s.sectionsContainer}>
           <div className={s.section}>
-            <Commitments />
-            <Subscriptions />
+            <div className={s.sectionSubHeader}>Find volunteer opportunities</div>
+            <div className={s.sectionInnerContent}>
+              <Link to="/search" className={s.flatButton}>
+                Search Opportunities <span>Find something that suits you</span>
+              </Link>
+              <Link to="/search/search-campaigns" className={s.flatButton}>
+                Search Campaigns <span>Show your support!</span>
+              </Link>
+            </div>
+
+            <div className={s.sectionSubHeader}>Manage Volunteer Opportunities</div>
+            <div className={s.sectionInnerContent}>
+              <Link to="/volunteer/opportunity-commitments" className={s.flatButton}>
+                My Opportunities <span>Opportunities you've volunteered for</span>
+              </Link>
+              <Link to="/volunteer/campaign-subscriptions" className={s.flatButton}>
+                Followed Campaigns <span>Be ready for new opportunities!</span>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
-
 
 export default Volunteer;
